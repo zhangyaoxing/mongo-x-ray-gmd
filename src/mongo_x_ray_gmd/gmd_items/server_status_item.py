c995a40b243a137b6bb2933d46efcc627256ea6f
Yaoxing Zhang <yaoxing.zhang@gmail.com>
2026-08-26 15:51:02 +0200

    feat: alert on FTDC configuration issues
    Apply the new FtdcRule from mongo-x-ray-hc to the server_parameters output
    on mongod nodes: diagnosticDataCollectionEnabled=false -> HIGH, and
    diagnosticDataCollectionSamplesPerChunk below 300 -> MEDIUM.
    
    Fixes #8

--- src/mongo_x_ray_gmd/gmd_items/server_status_item.py
+++ src/mongo_x_ray_gmd/gmd_items/server_status_item.py
@@ -6,6 +6,7 @@ from mongo_x_ray_hc.parsers.conn_parser import ConnParser
 from mongo_x_ray_hc.parsers.query_targeting_parser import QueryTargetingParser
 from mongo_x_ray_hc.rules.cache_rule import CacheRule
 from mongo_x_ray_hc.rules.connections_rule import ConnectionsRule
+from mongo_x_ray_hc.rules.ftdc_rule import FtdcRule
 from mongo_x_ray_hc.rules.query_targeting_rule import QueryTargetingRule
 from mongo_x_ray_hc.rules.sbe_rule import SbeRule
 from mongo_x_ray_hc.rules.snapshot_window_rule import SnapshotWindowRule
@@ -30,6 +31,7 @@ class ServerStatusItem(BaseItem):
         self._rules["write_concern"] = WriteConcernRule(config)
         self._rules["snapshot_window"] = SnapshotWindowRule(config)
         self._rules["sbe"] = SbeRule(config)
+        self._rules["ftdc"] = FtdcRule(config)
 
         def get_server_status(block):
             self._server_status = block.get("output", {})
@@ -58,6 +60,10 @@ class ServerStatusItem(BaseItem):
                 test_result, _ = self._rules["sbe"].apply(
                     self._server_parameters or {},
                     extra_info={"host": self._hostname, "version": self._server_version},
+                )
+                self.append_test_results(test_result)
+                test_result, _ = self._rules["ftdc"].apply(
+                    self._server_parameters or {}, extra_info={"host": self._hostname}
                 )
                 self.append_test_results(test_result)
             test_result, self._connections = self._rules["connections"].apply(
